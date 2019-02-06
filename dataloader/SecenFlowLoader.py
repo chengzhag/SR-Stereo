f1191856b7be38b08e7a53a405e986d7572170de
Unknown <850576570@qq.com>
2019-02-06 17:45:14 +0800

    Added full .pfm I/Ocode from SceneFlow.

--- dataloader/SecenFlowLoader.py
+++ dataloader/SecenFlowLoader.py
@@ -7,7 +7,7 @@ import random
 from PIL import Image, ImageOps
 from utils import preprocess
 from dataloader import listflowfile as lt
-from utils import readpfm as rp
+from utils import python_pfm as pfm
 import numpy as np
 
 IMG_EXTENSIONS = [
@@ -22,7 +22,7 @@ def default_loader(path):
     return Image.open(path).convert('RGB')
 
 def disparity_loader(path):
-    return rp.readPFM(path)
+    return pfm.readPFM(path)
 
 
 class myImageFloder(data.Dataset):
