5d08ffa8b0964b6770c800f6c0d2c9bc7692e13e
Cheng Zhang <850576570@qq.com>
2019-02-28 21:24:07 +0800

    Added module test Stereo_eval to moduletest.sh.

--- moduletest.sh
+++ moduletest.sh
@@ -14,6 +14,10 @@ PYTHONPATH=./ python train/Stereo_train.py --outputFolder moduletests --dispscal
 echo 'test Stereo_train/eval with PSMNetDown'
 PYTHONPATH=./ python train/Stereo_train.py --outputFolder moduletests --dispscale 0.5 --datapath $carla_kitti_dataset --dataset carla_kitti --load_scale 0.25 0.125 --trainCrop 128 256 --epochs 3 --log_every 1 --test_every 1 --eval_fcn outlier --batchsize_train 4 --batchsize_test 2 --lr 0.001 --lossWeights --model PSMNetDown --loadmodel logs/pretrained/PSMNet_pretrained_sceneflow/PSMNet_pretrained_sceneflow.tar
 
+# test Stereo_eval with PSMNet
+echo 'test Stereo_eval with PSMNet'
+PYTHONPATH=./ python evaluation\Stereo_eval.py --outputFolder moduletests --outputFolder moduletests --dispscale 0.5 --datapath /media/omnisky/zcSSD/SR-Stereo/datasets/carla_kitti/carla_kitti_sr_lowquality_moduletest/ --dataset carla_kitti --load_scale 0.25 --eval_fcn outlier --batchsize_test 2 --model PSMNet --loadmodel logs/moduletests/Stereo_train/190228164923_PSMNet_loadScale_2_trainCrop_128_256_batchSize_4_carla_kitti/checkpoint_epoch_0003_it_00002.tar --logToNew
+
 # test SR_train/eval with EDSR
 echo 'SR_train/eval with EDSR'
 PYTHONPATH=./ python train/SR_train.py --outputFolder moduletests --datapath $carla_kitti_dataset --dataset carla_kitti --load_scale 0.5 --trainCrop 128 256 --epochs 5 --log_every 1 --test_every 1 --eval_fcn l1 --batchsize_train 4 --batchsize_test 2  --lr 0.0001 --model SR --loadmodel logs/pretrained/EDSR_pretrained_DIV2K/EDSR_baseline_x2.pt
