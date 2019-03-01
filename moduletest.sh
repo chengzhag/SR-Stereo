9999a2dc89bad100e2a49067eec4d0ba56d5bed4
Cheng Zhang <850576570@qq.com>
2019-03-01 10:57:28 +0800

    Fixed peoblem with moduletest.sh. Simplified experiments.sh.

--- moduletest.sh
+++ moduletest.sh
@@ -4,7 +4,7 @@ carla_kitti_dataset=/media/omnisky/zcSSD/SR-Stereo/datasets/carla_kitti/carla_ki
 sceneflow_dataset=/media/omnisky/zcSSD/SR-Stereo/datasets/sceneflow/
 kitti2015_dataset=/media/omnisky/zcSSD/SR-Stereo/datasets/kitti/data_scene_flow/training/
 kitti2012_dataset=/media/omnisky/zcSSD/SR-Stereo/datasets/kitti/data_stereo_flow/training/
-export CUDA_VISIBLE_DEVICES=0,1
+export CUDA_VISIBLE_DEVICES=0
 
 # test Stereo_train/eval with PSMNet
 echo 'test Stereo_train/eval with PSMNet'
@@ -12,11 +12,11 @@ PYTHONPATH=./ python train/Stereo_train.py --outputFolder moduletests --dispscal
 
 # test Stereo_train/eval with PSMNet
 echo 'test Stereo_train/eval with PSMNetDown'
-PYTHONPATH=./ python train/Stereo_train.py --outputFolder moduletests --dispscale 0.5 --datapath $carla_kitti_dataset --dataset carla_kitti --load_scale 0.25 0.125 --trainCrop 128 256 --epochs 3 --log_every 1 --test_every 1 --eval_fcn outlier --batchsize_train 4 --batchsize_test 2 --lr 0.001 --lossWeights --model PSMNetDown --loadmodel logs/pretrained/PSMNet_pretrained_sceneflow/PSMNet_pretrained_sceneflow.tar
+PYTHONPATH=./ python train/Stereo_train.py --outputFolder moduletests --dispscale 0.5 --datapath $carla_kitti_dataset --dataset carla_kitti --load_scale 0.25 0.125 --trainCrop 128 256 --epochs 3 --log_every 1 --test_every 1 --eval_fcn outlier --batchsize_train 4 --batchsize_test 2 --lr 0.001 --lossWeights 0.5 0.5 --model PSMNetDown --loadmodel logs/pretrained/PSMNet_pretrained_sceneflow/PSMNet_pretrained_sceneflow.tar
 
 # test Stereo_eval with PSMNet
 echo 'test Stereo_eval with PSMNet'
-PYTHONPATH=./ python evaluation/Stereo_eval.py --outputFolder moduletests --outputFolder moduletests --dispscale 0.5 --datapath /media/omnisky/zcSSD/SR-Stereo/datasets/carla_kitti/carla_kitti_sr_lowquality_moduletest/ --dataset carla_kitti --load_scale 0.25 --eval_fcn outlier --batchsize_test 2 --model PSMNet --loadmodel logs/moduletests/Stereo_train/190228164923_PSMNet_loadScale_2_trainCrop_128_256_batchSize_4_carla_kitti/checkpoint_epoch_0003_it_00002.tar --logToNew
+PYTHONPATH=./ python evaluation/Stereo_eval.py --outputFolder moduletests --dispscale 0.5 --datapath $carla_kitti_dataset --dataset carla_kitti --load_scale 0.25 --eval_fcn outlier --batchsize_test 2 --model PSMNet --loadmodel logs/pretrained/PSMNet_pretrained_sceneflow/PSMNet_pretrained_sceneflow.tar --logToNew
 
 # test SR_train/eval with EDSR
 echo 'SR_train/eval with EDSR'
@@ -25,7 +25,4 @@ PYTHONPATH=./ python train/SR_train.py --outputFolder moduletests --datapath $ca
 # test SRdisp_train/eval with EDSR
 echo 'test SRdisp_train/eval with EDSR'
 PYTHONPATH=./ python train/SR_train.py --outputFolder moduletests --datapath $carla_kitti_dataset --dataset carla_kitti --load_scale 0.5 --trainCrop 128 256 --epochs 5 --log_every 1 --test_every 1 --eval_fcn l1 --batchsize_train 4 --batchsize_test 2  --lr 0.0001 --model SRdisp --loadmodel logs/pretrained/EDSR_pretrained_DIV2K/EDSR_baseline_x2.pt
-
-
-
 
