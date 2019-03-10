8bd9b1c46019e5a755f514f530f65e894486b359
Cheng Zhang <850576570@qq.com>
2019-03-10 19:03:15 +0800

    Test before the first epoch by default.

--- train/Train.py
+++ train/Train.py
@@ -118,7 +118,7 @@ class Train:
             # test
             if ((self.testEvery > 0 and epoch > 0 and epoch % self.testEvery == 0)
                 or (self.testEvery in (0, -2) and epoch == self.nEpochs)
-                or (self.testEvery == -2 and epoch == 0)) \
+                or (self.testEvery not in (0, -1) and epoch == 0)) \
                     and self.test is not None:
                 testScores = self.test(model=self.model, global_step=self.global_step).values()
                 testScore = sum(testScores) / len(testScores)
